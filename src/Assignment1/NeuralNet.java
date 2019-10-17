272a261155e6ea0125e8ffc16ee2b1cc459d5aa2
Jiahao1995 <lijiahao5269@outlook.com>
2019-10-16 19:35:21 -0700

    add Main class

--- src/Assignment1/NeuralNet.java
+++ src/Assignment1/NeuralNet.java
@@ -149,19 +149,5 @@ public class NeuralNet implements NeuralNetInterface {
         }
     }
 
-    public static void main(String[] args) {
-        NeuralNet xor = new NeuralNet(2, 4, 0.2, 0.9, 0, 1);
-        int epoch = 0;
 
-//        for (int i = 0; i < 100; i++) {
-//            epoch += xor.train(new double[][]{{0, 0}, {0, 1}, {1, 0}, {1, 1}}, new double[][]{{0}, {1}, {1}, {0}});
-//        }
-
-        for (int i = 0; i < 100; i++) {
-            epoch += xor.train(new double[][]{{-1, -1}, {-1, 1}, {1, -1}, {1, 1}},
-                    new double[][]{{-1}, {1}, {1}, {-1}});
-        }
-
-        System.out.println(epoch / 100);
-    }
 }
